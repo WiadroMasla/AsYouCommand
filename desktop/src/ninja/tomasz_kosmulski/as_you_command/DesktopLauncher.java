71cf290a1d888c491abd07c7d65c6ee5f26394be
WiadroMasla <tomasz.kosmulski@student.uj.edu.pl>
2024-02-27 14:37:29 +0100

    Init commit

---
+++ desktop/src/ninja/tomasz_kosmulski/as_you_command/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ninja.tomasz_kosmulski.as_you_command;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ninja.tomasz_kosmulski.as_you_command.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("My GDX Game");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
